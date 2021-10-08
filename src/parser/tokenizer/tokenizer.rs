71be6ab94f94d7a07e75c24ac7029e9f5e02cd8b
Shinyaigeek <Philispaxil@gmail.com>
2021-10-09 05:47:23 +0900

    Update: test

--- src/parser/tokenizer/tokenizer.rs
+++ src/parser/tokenizer/tokenizer.rs
@@ -1,7 +1,26 @@
-pub fn tokenize(source: &str) -> Vec<&str> {
-    let mut tokens = vec![];
+use crate::parser::tokenizer::token::Token;
 
-    for char in source.chars() {}
+pub fn tokenize<'a>(source: &str) -> Vec<Token<'a>> {
+    let mut tokens: Vec<Token<'a>> = vec![];
+    let mut is_escaping = false;
+    let mut is_breaked = true;
+
+    for c in source.chars() {
+        if c == '\n' {
+            tokens.push(Token::Break);
+            is_breaked = true;
+            continue;
+        }
+        is_breaked = false;
+        if c == '\\' {
+            is_escaping = true;
+            continue;
+        }
+
+        if c == ' ' {
+            is_escaping = false;
+        }
+    }
 
     tokens
 }
@@ -11,8 +30,8 @@ mod test {
     use super::*;
 
     #[test]
-    fn tokenize_works() {
-        let tokens: Vec<&str> = vec![];
+    fn tokenize_works_with_empty_source() {
+        let tokens: Vec<Token<'static>> = vec![];
         assert_eq!(tokenize(""), tokens);
     }
 }
